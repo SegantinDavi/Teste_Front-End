025f6e58b5ac028d942759f4c160514605a4b192
Davi Segantin <davi.g.segantin@gmail.com>
2024-08-30 09:59:01 -0300

    correcao no script e adicao de novos metodos

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ const phoneView1 = document.querySelector('#phoneView1');
 const phoneView2 = document.querySelector('#phoneView2');
 const phoneExample = 11999999999;//exemplo para uso no método que formata o numero de telefone
 const phoneInput = document.querySelector('#phoneInput');
+const cpfInput = document.querySelector('#cpfInput');
+const btnSubmit = document.querySelector('#btnSubmit');
 
 phoneView1.addEventListener('click', () => {
     phoneView1.innerHTML = formatPhone(phoneExample);
@@ -11,9 +13,24 @@ phoneView2.addEventListener('click', () => {
     phoneView2.innerHTML = formatPhone(phoneExample);
 });
 
-phoneInput.addEventListener('keyup', () => {
-    const inputValue = phoneInput.value;
+phoneInput.addEventListener('keydown', (event) => {
+    numCheck(event);
+    let inputValue = phoneInput.value;
     phoneInput.value = phoneMask(inputValue);
+});
+
+cpfInput.addEventListener('keydown', (event) => {
+    numCheck(event);
+    let inputValue = cpfInput.value;
+    cpfInput.value = cpfMask(inputValue);
+});
+
+btnSubmit.addEventListener('click', () => {
+    if (formCheck()) {
+        alert('Mensagem eniada!');
+    } else {
+        alert('Preencha todos os campos');
+    }
 });
 
 function formatPhone(number) {
@@ -26,7 +43,30 @@ function formatPhone(number) {
 function phoneMask(digit) {
     if (!digit) return '';
     digit = digit.replace(/\D/g,'');
-    digit = digit.replace(/(\d{2})(\d)/,'($1) $2');
+    digit = digit.replace(/(\d{2})(\d)/,'($1)$2');
     digit = digit.replace(/(\d)(\d{4})$/,'$1-$2');
     return digit;
+}
+
+function cpfMask(digit) {
+    if (!digit) return '';
+    digit = digit.replace(/\D/g,'');
+    digit = digit.replace(/(\d{3})(\d)/,'$1.$2');
+    digit = digit.replace(/(\d{3})(\d)/,'$1.$2');
+    digit = digit.replace(/(\d{3})(\d{1,2})$/,'$1-$2');
+    return digit;
+}
+
+function numCheck(event) {
+    if (event.key.length === 1 && event.key.match(/[^0-9]/)) {
+        event.preventDefault(); 
+    }
+}
+
+function formCheck() {
+    let textInput = document.querySelectorAll('.form-input');
+    for (let i = 0; i < textInput.length; i++) {
+        if (textInput[i].value === '') return false;
+    }
+    return true;
 }
